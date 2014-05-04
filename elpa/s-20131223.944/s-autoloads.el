f9b3822ed2cfc5d9e9d55af9ec8ea24894db3d0f
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-05-05 05:55:20 +0900

    elpa, url, bookmarks, elispを管理対象から除外

--- elpa/s-20131223.944/s-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("s-pkg.el" "s.el") (21317 23528 27887
-;;;;;;  965000))
-
-;;;***
-
-(provide 's-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; s-autoloads.el ends here
