fc295a2203353537278f03a755ec86e2d584af8d
vitamiena <so.vita.oyasho@gmail.com>
2014-06-06 02:21:00 +0900

    .caskディレクトリを管理外に

--- .cask/24.3.1/elpa/rhtml-mode-20130422.611/rhtml-mode-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; rhtml-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (rhtml-mode) "rhtml-mode" "rhtml-mode.el" (21392
-;;;;;;  39846 218103 355000))
-;;; Generated autoloads from rhtml-mode.el
-
-(autoload 'rhtml-mode "rhtml-mode" "\
-Embedded Ruby Mode (RHTML)
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.html\\.erb$" . rhtml-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("rhtml-erb.el" "rhtml-fonts.el" "rhtml-mode-pkg.el"
-;;;;;;  "rhtml-navigation.el" "rhtml-ruby-hook.el" "rhtml-sgml-hacks.el")
-;;;;;;  (21392 39846 488239 808000))
-
-;;;***
-
-(provide 'rhtml-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; rhtml-mode-autoloads.el ends here
