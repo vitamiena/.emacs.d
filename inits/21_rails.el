96d315de52525466afe21ad25a3a7f33dd64ea8a
vitamiena <s13t210@stmail.eng.kagawa-u.ac.jp>
2014-11-17 11:56:16 +0900

    init-loaderを導入し、設定ファイルを分割

---
+++ inits/21_rails.el
@@ -0,0 +1,20 @@
+;;-------------------------------------------------------------------
+;; rails
+;;-------------------------------------------------------------------
+(require 'rhtml-mode)
+(require 'slim-mode)
+(require 'coffee-mode)
+;; projective-rails
+(require 'projectile)
+(projectile-global-mode)
+(require 'projectile-rails)
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
+(setq echo-keystrokes 0.1)
+(setq projectile-completion-system 'grizzl)
+
+(require 'recentf-ext)
+(setq recentf-max-saved-items 5000)
+
+
+;;
+(defun ruby-mode-set-encoding () ())
