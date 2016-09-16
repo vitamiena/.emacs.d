d681407af80208665c5e7affd9bee148c24428e3
vitamiena <so.vita.oyasho@gmail.com>
2016-09-16 20:33:38 +0900

    web-modeのインデントの調整

--- inits/13_web-mode.el
+++ inits/13_web-mode.el
@@ -2,23 +2,29 @@
 ;; web-mode
 ;;--------------------------------------------------------------------------------
 ;;; 適用する拡張子
+(require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js?$"     . web-mode))
 
 ;;; インデント数
-(defun web-mode-hook ()
+(defun my-web-mode-hook ()
   "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset   2)
   (setq web-mode-html-offset   2)
   (setq web-mode-css-offset    2)
   (setq web-mode-script-offset 2)
   (setq web-mode-php-offset    2)
   (setq web-mode-java-offset   2)
-  (setq web-mode-asp-offset    2))
-(add-hook 'web-mode-hook 'web-mode-hook)
+  (setq web-mode-asp-offset    2)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 ;; color:#ff0000;等とした場合に指定した色をbgに表示しない
 ;;(setq web-mode-disable-css-colorization t)
 ;;css,js,php,etc..の範囲をbg色で表示
